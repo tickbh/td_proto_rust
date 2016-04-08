4fc2375859acdad19568a2e054e6a36558f7a81e
tickbh <tickdream125@hotmail.com>
2016-04-08 10:42:05 +0800

    代码格式化

--- src/encode.rs
+++ src/encode.rs
@@ -1,4 +1,4 @@
-use std::io::{Write};
+use std::io::Write;
 use std::mem;
 
 use Config;
@@ -11,15 +11,15 @@ use {get_type_by_name, get_name_by_type, get_array_contains_type, get_type_by_va
 
 use STR_TYPE_NIL;
 
-static FILL_UP : [u8;16] = [0; 16];
+static FILL_UP: [u8; 16] = [0; 16];
 
-fn write_str_field(buffer : &mut Buffer, pattern : &str) -> RpResult<bool> {
+fn write_str_field(buffer: &mut Buffer, pattern: &str) -> RpResult<bool> {
     try!(encode_number(buffer, &Value::from(0 as u16)));
     try!(encode_number(buffer, &Value::U16(get_type_by_name(pattern))));
     Ok(true)
 }
 
-fn append_and_align(buffer : &mut Buffer, val : &[u8]) -> RpResult<()>  {
+fn append_and_align(buffer: &mut Buffer, val: &[u8]) -> RpResult<()> {
     let _add = match val.len() % 2 {
         0 => 0,
         val => 2 - val,
@@ -29,38 +29,38 @@ fn append_and_align(buffer : &mut Buffer, val : &[u8]) -> RpResult<()>  {
     Ok(())
 }
 
-pub fn encode_number(buffer : &mut Buffer, value : &Value) -> RpResult<()>  {
+pub fn encode_number(buffer: &mut Buffer, value: &Value) -> RpResult<()> {
     match *value {
         Value::U8(val) => {
-            try!(buffer.write(unsafe { &mem::transmute::<u8, [u8;1]>(val) }));
+            try!(buffer.write(unsafe { &mem::transmute::<u8, [u8; 1]>(val) }));
             try!(buffer.write(&FILL_UP[0..1]));
         }
         Value::I8(val) => {
-            try!(buffer.write(unsafe { &mem::transmute::<i8, [u8;1]>(val) }));
+            try!(buffer.write(unsafe { &mem::transmute::<i8, [u8; 1]>(val) }));
             try!(buffer.write(&FILL_UP[0..1]));
         }
         Value::U16(val) => {
-            try!(buffer.write(unsafe { &mem::transmute::<u16, [u8;2]>(val.to_le()) }));
+            try!(buffer.write(unsafe { &mem::transmute::<u16, [u8; 2]>(val.to_le()) }));
         }
         Value::I16(val) => {
-            try!(buffer.write(unsafe { &mem::transmute::<i16, [u8;2]>(val.to_le()) }));
+            try!(buffer.write(unsafe { &mem::transmute::<i16, [u8; 2]>(val.to_le()) }));
         }
         Value::U32(val) => {
-            try!(buffer.write(unsafe { &mem::transmute::<u32, [u8;4]>(val.to_le()) }));
+            try!(buffer.write(unsafe { &mem::transmute::<u32, [u8; 4]>(val.to_le()) }));
         }
         Value::I32(val) => {
-            try!(buffer.write(unsafe { &mem::transmute::<i32, [u8;4]>(val.to_le()) }));
+            try!(buffer.write(unsafe { &mem::transmute::<i32, [u8; 4]>(val.to_le()) }));
         }
         Value::Float(val) => {
             let val = (val * 1000.0) as i32;
-            try!(buffer.write(unsafe { &mem::transmute::<i32, [u8;4]>(val.to_le()) }));
+            try!(buffer.write(unsafe { &mem::transmute::<i32, [u8; 4]>(val.to_le()) }));
         }
-        _ => unreachable!("encode_number only")
+        _ => unreachable!("encode_number only"),
     }
     Ok(())
 }
 
-pub fn encode_str_raw(buffer : &mut Buffer, value : &Value) -> RpResult<()>  {
+pub fn encode_str_raw(buffer: &mut Buffer, value: &Value) -> RpResult<()> {
     match *value {
         Value::Str(ref val) => {
             try!(encode_number(buffer, &Value::U16(val.len() as u16)));
@@ -70,28 +70,28 @@ pub fn encode_str_raw(buffer : &mut Buffer, value : &Value) -> RpResult<()>  {
             try!(encode_number(buffer, &Value::U16(val.len() as u16)));
             try!(append_and_align(buffer, &val[..]));
         }
-        _ => unreachable!("encode_number only")
+        _ => unreachable!("encode_number only"),
     }
     Ok(())
 }
 
-pub fn encode_map(buffer : &mut Buffer, config : &Config, value : &Value) -> RpResult<()>  {
+pub fn encode_map(buffer: &mut Buffer, config: &Config, value: &Value) -> RpResult<()> {
     match *value {
         Value::Map(ref val) => {
             for (name, sub_value) in val {
                 if try!(write_field(buffer, config.get_field_by_name(name))) {
-                    try!(encode_field(buffer, config, sub_value));    
+                    try!(encode_field(buffer, config, sub_value));
                 }
             }
             try!(write_str_field(buffer, STR_TYPE_NIL));
         }
-        _ => unreachable!("encode_number only")
+        _ => unreachable!("encode_number only"),
     }
     Ok(())
 }
 
 
-pub fn write_field(buffer : &mut Buffer, field : Option<&Field>) -> RpResult<bool> {
+pub fn write_field(buffer: &mut Buffer, field: Option<&Field>) -> RpResult<bool> {
     if field.is_none() {
         return Ok(false);
     }
@@ -101,10 +101,16 @@ pub fn write_field(buffer : &mut Buffer, field : Option<&Field>) -> RpResult<boo
     Ok(true)
 }
 
-pub fn encode_field(buffer : &mut Buffer, config : &Config, value : &Value) -> RpResult<()> {
+pub fn encode_field(buffer: &mut Buffer, config: &Config, value: &Value) -> RpResult<()> {
     try!(write_str_field(buffer, get_name_by_type(get_type_by_value(value))));
     match *value {
-        Value::U8(_) | Value::I8(_) | Value::U16(_) | Value::I16(_) | Value::U32(_) | Value::I32(_) | Value::Float(_)  => {
+        Value::U8(_) |
+        Value::I8(_) |
+        Value::U16(_) |
+        Value::I16(_) |
+        Value::U32(_) |
+        Value::I32(_) |
+        Value::Float(_) => {
             try!(encode_number(buffer, value));
         }
         Value::Str(_) | Value::Raw(_) => {
@@ -113,11 +119,17 @@ pub fn encode_field(buffer : &mut Buffer, config : &Config, value : &Value) -> R
         Value::Map(_) => {
             try!(encode_map(buffer, config, value));
         }
-        Value::Nil => {
-        }
-        Value::AU8(ref val) | Value::AI8(ref val) | Value::AU16(ref val) | Value::AI16(ref val) | 
-        Value::AU32(ref val) | Value::AI32(ref val) | Value::AFloat(ref val) | Value::AStr(ref val) |
-        Value::ARaw(ref val) | Value::AMap(ref val) => {
+        Value::Nil => {}
+        Value::AU8(ref val) |
+        Value::AI8(ref val) |
+        Value::AU16(ref val) |
+        Value::AI16(ref val) |
+        Value::AU32(ref val) |
+        Value::AI32(ref val) |
+        Value::AFloat(ref val) |
+        Value::AStr(ref val) |
+        Value::ARaw(ref val) |
+        Value::AMap(ref val) => {
             let must_type = get_array_contains_type(value);
             for v in val {
                 check_vailed!(v, must_type);
@@ -129,17 +141,22 @@ pub fn encode_field(buffer : &mut Buffer, config : &Config, value : &Value) -> R
     Ok(())
 }
 
-pub fn encode_proto(buffer : &mut Buffer, config : &Config, name : &String, infos : Vec<Value>) -> RpResult<()> {
+pub fn encode_proto(buffer: &mut Buffer,
+                    config: &Config,
+                    name: &String,
+                    infos: Vec<Value>)
+                    -> RpResult<()> {
     let proto = config.get_proto_by_name(name);
-    ensure!(proto.is_some(), (ErrorKind::MissingError, "missing the name protocol"));
+    ensure!(proto.is_some(),
+            (ErrorKind::MissingError, "missing the name protocol"));
     let proto = proto.unwrap();
-    ensure!(proto.args.len() == infos.len(), (ErrorKind::TypeNotMatchError, "the data num not match protocol args num"));
+    ensure!(proto.args.len() == infos.len(),
+            (ErrorKind::TypeNotMatchError, "the data num not match protocol args num"));
     try!(encode_str_raw(buffer, &Value::Str(name.clone())));
     for info in &infos {
-        //TODO match args
+        // TODO match args
         try!(encode_field(buffer, config, info));
     }
     try!(write_str_field(buffer, STR_TYPE_NIL));
     Ok(())
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,6 @@ pub mod decode;
 pub use values::*;
 pub use config::{Config, Field, Proto};
 pub use buffer::Buffer;
-pub use encode::{encode_proto, encode_field, write_field, encode_number, encode_map, encode_str_raw};
-pub use decode::{decode_proto, decode_field, read_field , decode_number, decode_map, decode_str_raw};
-
+pub use encode::{encode_proto, encode_field, write_field, encode_number, encode_map,
+                 encode_str_raw};
+pub use decode::{decode_proto, decode_field, read_field, decode_number, decode_map, decode_str_raw};
